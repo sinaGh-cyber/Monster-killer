55c48d75bc77a351f15de8a2470811eda48cd6d6
sinaGh-cyber <sinaina1377@gmail.com>
2021-05-08 01:58:26 +0430

    monster killer attack handler fuction added v2.00

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,20 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUe = 17;
 const MONSTER_ATTACK_VALUE = 14;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
-function resultCheck(currentMonsterHealth, currentPlayerHealth) {
+function attackMonster(mode) {
+  let maxDamage;
+  if ( mode === `ATTACK` ){
+      maxDamage = ATTACK_VALUE;
+  } else if ( mode === `STRONG_ATTACK` ) {
+      maxDamage = STRONG_ATTACK_VALUe;
+  }
+    const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert("your Won!");
   } else if (currentMonsterHealth >= 0 && currentPlayerHealth <= 0) {
@@ -15,10 +26,10 @@ function resultCheck(currentMonsterHealth, currentPlayerHealth) {
 adjustHealthBars(chosenMaxLife);
 
 function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
-  playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage;
-  resultCheck(currentMonsterHealth, currentPlayerHealth);
+attackMonster(`ATTACK`)
+}
+function strongAttackHandler() {
+  attackMonster(`STRONG_ATTACK`)
 }
 attackBtn.addEventListener(`click`, attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
