8364ce2c5c2abe88ea9f4a2b10698086e8ebbb87
sinaGh-cyber <sinaina1377@gmail.com>
2021-05-10 22:23:01 +0430

    monster killer_ battle log modified eith for loops  v7.00

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,14 +3,15 @@ const STRONG_ATTACK_VALUe = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-const MODE_ATTACK = 'ATTACK'; // MODE_ATTACK = 0
-const MODE_STRONG_ATTACK = 'STRONG_ATTACK'; // MODE_STRONG_ATTACK = 1
-const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
-const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
-const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
-const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
-const LOG_EVENT_GAME_OVER = 'GAME_OVER';
+const MODE_ATTACK = "ATTACK"; // MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = "STRONG_ATTACK"; // MODE_STRONG_ATTACK = 1
+const LOG_EVENT_PLAYER_ATTACK = "PLAYER_ATTACK";
+const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
+const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
+const LOG_EVENT_PLAYER_HEAL = "PLAYER_HEAL";
+const LOG_EVENT_GAME_OVER = "GAME_OVER";
 let battleLog = [];
+let logEntry = {};
 
 let chosenMaxLife = 100;
 let enteredMaxLife = prompt(`Enter your chosen max life: `, `${chosenMaxLife}`);
@@ -47,7 +48,7 @@ function damageDealer() {
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'PLAYER WON',
+      "PLAYER WON",
       currentMonsterHealth,
       currentPlayerHealth
     );
@@ -55,7 +56,7 @@ function damageDealer() {
   } else if (currentMonsterHealth >= 0 && currentPlayerHealth <= 0) {
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'MONSTER WON',
+      "MONSTER WON",
       currentMonsterHealth,
       currentPlayerHealth
     );
@@ -63,7 +64,7 @@ function damageDealer() {
   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     writeToLog(
       LOG_EVENT_GAME_OVER,
-      'A DRAW',
+      "A DRAW",
       currentMonsterHealth,
       currentPlayerHealth
     );
@@ -87,41 +88,41 @@ function attackMonster(mode) {
 adjustHealthBars(chosenMaxLife);
 
 function writeToLog(ev, val, monsterHealth, playerHealth) {
-  let logEntry = {
+  logEntry = {
     event: ev,
     value: val,
     finalMonsterHealth: monsterHealth,
-    finalPlayerHealth: playerHealth
+    finalPlayerHealth: playerHealth,
   };
   switch (ev) {
     case LOG_EVENT_PLAYER_ATTACK:
-      logEntry.target = 'MONSTER';
+      logEntry.target = "MONSTER";
       break;
     case LOG_EVENT_PLAYER_STRONG_ATTACK:
       logEntry = {
         event: ev,
         value: val,
-        target: 'MONSTER',
+        target: "MONSTER",
         finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
+        finalPlayerHealth: playerHealth,
       };
       break;
     case LOG_EVENT_MONSTER_ATTACK:
       logEntry = {
         event: ev,
         value: val,
-        target: 'PLAYER',
+        target: "PLAYER",
         finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
+        finalPlayerHealth: playerHealth,
       };
       break;
     case LOG_EVENT_PLAYER_HEAL:
       logEntry = {
         event: ev,
         value: val,
-        target: 'PLAYER',
+        target: "PLAYER",
         finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
+        finalPlayerHealth: playerHealth,
       };
       break;
     case LOG_EVENT_GAME_OVER:
@@ -129,7 +130,7 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
         event: ev,
         value: val,
         finalMonsterHealth: monsterHealth,
-        finalPlayerHealth: playerHealth
+        finalPlayerHealth: playerHealth,
       };
       break;
     default:
@@ -172,7 +173,6 @@ function writeToLog(ev, val, monsterHealth, playerHealth) {
   battleLog.push(logEntry);
 }
 
-
 function attackHandler() {
   attackMonster(MODE_ATTACK);
 }
@@ -192,10 +192,19 @@ function healHandler() {
   damageDealer();
 }
 function printLogHandler() {
-  console.log(battleLog);
+  let i = 0;
+  console.log(`---------------------------------------------------------`);
+  for (const elememnt of battleLog) {
+    console.log(`#${i}`);
+    for (const praperty in logEntry) {
+      console.log(`* ${praperty} => ${logEntry[praperty]} *`);
+    }
+    console.log(`---------------------------------------------------------`);
+    i++;
+  }
 }
 
 attackBtn.addEventListener(`click`, attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener(`click`, healHandler);
-logBtn.addEventListener('click', printLogHandler);
+logBtn.addEventListener("click", printLogHandler);
